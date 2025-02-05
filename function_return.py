714d0df97e0e3992d45791d977553a6771be502c
ArriXtti <isviridyuk@gmail.com>
2025-02-05 15:25:04 +0200

    Return information from a function

---
+++ function_return.py
@@ -0,0 +1,6 @@
+#Return information from a function
+def calculate_fails(total_attempts, failed_attempts):
+    fail_percentage = failed_attempts / total_attempts
+    return fail_percentage
+
+calculate_fails(4,2)
